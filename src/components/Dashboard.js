f17285409fdb57c8624f0bb29b376436cd73fc17
Bob <rsobieszek89@gmail.com>
2019-10-01 12:36:25 +0200

    backup 01

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import UsersList from './UsersList'
+import { NavLink } from 'react-router-dom'
 
 const Dashboard = () => {
-    return(
+    return (
         <div className="container">
-            <h1>Dashboard</h1>
-            <UsersList />
+            <div className="row">
+                <h1>Dashboard</h1>
+                <NavLink to='/add' className="waves-effect waves-light btn">
+                    Add user
+                </NavLink>
+            </div>
+            <div className="row">
+                <UsersList />
+            </div>
         </div>
     )
 }
